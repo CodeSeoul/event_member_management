bd515da909cc68934262913cdc06251e96971fbd
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2023-05-29 17:53:30 +0900

    chore: Fixed console logs and load types

--- src/routes/events/+page.ts
+++ src/routes/events/+page.ts
@@ -1,7 +1,7 @@
-import type { PageLoad } from '$types/src/routes/events/$types';
 import type { EventListData } from '$lib/EventData';
+import type { LoadEvent } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ fetch }) => {
+export async function load({ fetch }: LoadEvent): Promise<EventListData | null> {
 	const url = 'http://localhost:8000/events';
 	const options = {
 		method: 'GET',
@@ -15,10 +15,10 @@ export const load: PageLoad = async ({ fetch }) => {
 		response = await fetch(url, options);
 	} catch (error) {
 		console.error(`Failed to retrieve event list due to error: ${error}`);
-		return;
+		return null;
 	}
 	const data = await response.json();
-	console.log(data);
+	console.debug(data);
 	const eventList = data._embedded;
 	const outputList: EventListData = {
 		events: []
@@ -48,27 +48,4 @@ export const load: PageLoad = async ({ fetch }) => {
     updatedAt: "2023-03-19T13:40:59.758617Z"
     venue: "Yo mama's house"
      */
-
-	// return {
-	//     events: [
-	//         {
-	//             id: 1,
-	//             eventTitle: "The Biggest Event!",
-	//             eventDate: new Date(),
-	//             eventTimeslot: "Saturday Hangouts",
-	//             eventSeriesName: "The Best Events",
-	//             eventDescription: "It is the biggest event. All the coolest people will be there. FOMO at max levels.",
-	//             eventImage: "https://lh3.googleusercontent.com/u/0/drive-viewer/AFGJ81ruKlLCVFVls3Q0laC93lRTNvmrzPFmR1FyxkrdhTN364lSJqctcNMjHZXC5jWz_JLaeXIYISEj0UBFIQRdQlZuDsoiAA=w1920-h929"
-	//         },
-	//         {
-	//             id: 2,
-	//             eventTitle: "Another Great Event!!",
-	//             eventDate: new Date(),
-	//             eventTimeslot: "Machine Learning Afternoons",
-	//             eventSeriesName: null,
-	//             eventDescription: "This one's great, too! This description needs to be really log to trigger the overflow logic. How long? I have no idea! I'm just going to ramble until I think there's sufficient madness. Madness?! THIS IS SPARTA! But not because Sparta was quite violent. However, the mighty feels are so mighty. So so mighty. I should work out, so I can look like Leonidus.",
-	//             eventImage: "https://lh3.googleusercontent.com/u/0/drive-viewer/AFGJ81ruKlLCVFVls3Q0laC93lRTNvmrzPFmR1FyxkrdhTN364lSJqctcNMjHZXC5jWz_JLaeXIYISEj0UBFIQRdQlZuDsoiAA=w1920-h929"
-	//         }
-	//     ]
-	// };
-};
+}
--- src/routes/events/[slug]/+page.ts
+++ src/routes/events/[slug]/+page.ts
@@ -1,17 +1,17 @@
 import type { EventDetailData } from '$lib/EventData';
 import type { LoadEvent } from '@sveltejs/kit';
 
-export async function load(loadData: LoadEvent): Promise<EventDetailData | null> {
+export async function load({ params, fetch }: LoadEvent): Promise<EventDetailData | null> {
 	// {"route":{"id":"/events/[slug]"},"params":{"slug":"3"},"data":null,"url":"http://localhost:5173/events/3"}
-	console.debug(`load input: ${JSON.stringify(loadData)}`);
-	if (!loadData.params.slug) {
+	console.debug(`load params: ${JSON.stringify(params)}`);
+	if (!params.slug) {
 		throw new Error('No event ID passed to event detail page');
 	}
-	const eventId: number = parseInt(loadData.params.slug, 10);
+	const eventId: number = parseInt(params.slug, 10);
 	if (isNaN(eventId)) {
 		throw new Error('Event ID was not a number');
 	}
-	console.log(`load event id: ${eventId}`);
+	console.debug(`load event id: ${eventId}`);
 	const url = `http://localhost:8000/events/${eventId}`;
 	const options = {
 		method: 'GET',
@@ -27,7 +27,7 @@ export async function load(loadData: LoadEvent): Promise<EventDetailData | null>
 		throw new Error(`Failed to retrieve event list due to error: ${error}`);
 	}
 	const data = await response.json();
-	console.log(data);
+	console.debug(data);
 	return {
 		id: data.id,
 		title: data.title,
