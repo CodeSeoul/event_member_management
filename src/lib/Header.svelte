ba853e6aacc6ebde9c98d70f06d79e1758de20b0
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2023-06-04 18:21:24 +0900

    feat: Now with EvenTer logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,9 +6,9 @@
 <div class="border-b-[0.2rem] border-b-[#ED1B24] h-20">
 	<div class="flex m-auto w-3/5 items-center justify-between">
 		<img
-			class="w-[14rem] align-middle border-none"
-			src="/logo_small.png"
-			alt="Small CodeSeoul Logo"
+			class="max-w-56 max-h-44 mt-1 align-middle border-none"
+			src="/logo_small.svg"
+			alt="Small EvenTer Logo"
 		/>
 		<Navbar />
 	</div>
